8cae1dd68279f884db02c061bd11d19587a8ad10
estebanjabad <estebanjabad@gmail.com>
2024-01-27 22:33:41 -0300

    forwarding unit andando

--- forwarding.sv
+++ forwarding.sv
@@ -8,13 +8,13 @@ module forwarding(input logic [4:0] ID_EX_reg_rm, ID_EX_reg_rn,
 
         if ( MEM_WB_regWrite && (MEM_WB_reg_rd != 31) &&
             !((EX_MEM_regWrite && (EX_MEM_reg_rd != 31)) &&
-            (EX_MEM_reg_rd != ID_EX_reg_rn)) &&
+            (EX_MEM_reg_rd == ID_EX_reg_rn)) &&
             (MEM_WB_reg_rd == ID_EX_reg_rn) ) forwardA = 2'b01;
 
         if ( MEM_WB_regWrite &&
             (MEM_WB_reg_rd != 31) &&
             !( (EX_MEM_regWrite && (EX_MEM_reg_rd != 31)) &&
-               (EX_MEM_reg_rd != ID_EX_reg_rm) ) &&
+               (EX_MEM_reg_rd == ID_EX_reg_rm) ) &&
             (MEM_WB_reg_rd == ID_EX_reg_rm)) forwardB = 2'b01;
 
         if (  EX_MEM_regWrite && 
